52e0ce9286fb3bcecd3d712a4d71eebf0d060695
Peiliang Guo <edwardplguo@gmail.com>
2017-01-24 20:58:42 -0500

    Initial Commit

---
+++ spell/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  spell
+//
+//  Created by Edward Guo on 2017-01-24.
+//  Copyright © 2017 Pei Liang Guo. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
